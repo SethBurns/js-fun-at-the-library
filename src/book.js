178a85342930297817f7d7dffb6ab9cc52513e4f
Seth Burns <seth@sethburns.com>
2023-03-23 16:53:27 -0700

    will finish the book.js assignment

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+// @ts-check
+function createTitle(title) {
+  return `The ${title}`
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+};
+
+function saveReview(newReview, reviews) {
+  if (reviews.indexOf(newReview) > -1) {
+  } else {
+    return reviews.push(newReview)
+    }
+  }; 
+
+function calculatePageCount(title) {
+  return title.length * 20
+};
+
+function writeBook(title, bookCharacter, genre) {
+  var book = {}
+  book.title = title
+  book.mainCharacter = bookCharacter
+  book.pageCount = calculatePageCount(book.title)
+  book.genre = genre
+  return book
+};
+
+function editBook(bookObj) {
+  bookObj.pageCount = bookObj.pageCount * .75
 }
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
